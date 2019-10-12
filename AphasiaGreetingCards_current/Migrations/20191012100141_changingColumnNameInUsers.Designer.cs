3c8f2b268956f93286612a821775ebd9c6035f36
psivi <psivi92@gmail.com>
2019-10-12 13:11:10 +0300

    stam

--- AphasiaGreetingCards_current/Migrations/20191012100141_changingColumnNameInUsers.Designer.cs
+++ AphasiaGreetingCards_current/Migrations/20191012100141_changingColumnNameInUsers.Designer.cs
@@ -4,14 +4,16 @@ using AphasiaGreetingCards.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AphasiaGreetingCards.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191012100141_changingColumnNameInUsers")]
+    partial class changingColumnNameInUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
