9ed5241617a5b7689c800f33f20a42a811fb9476
psivi <psivi92@gmail.com>
2019-10-12 10:01:23 +0300

    Word Statistics init

---
+++ AphasiaGreetingCards_current/Controllers/StatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AphasiaGreetingCards.Data;
+using AphasiaGreetingCards.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AphasiaGreetingCards.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StatisticsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var all = _context.GreetingCards.ToList();
+            string[] strings = new string[all.Count];
+            for (int i = 0; i < all.Count; i++)
+            {
+                strings[i] = all.ElementAt(i).fullSentence;
+            }
+            ViewData["Strings"] = strings;
+            ViewBag.StringsCount = all.Count;
+            return View();
+        }
+        private Dictionary<string,int> CountWords()
+        {
+
+
+            return null;
+        }
+    }
+}
