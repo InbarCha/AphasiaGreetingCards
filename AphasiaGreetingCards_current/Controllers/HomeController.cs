dfa77376bad4de852a1b831e5753ea338fa34b6b
InbarCha <Inbar@LAPTOP-IDK7GB0E>
2019-10-14 19:42:45 +0300

    Adding Canvas (in home page)

--- AphasiaGreetingCards_current/Controllers/HomeController.cs
+++ AphasiaGreetingCards_current/Controllers/HomeController.cs
@@ -15,16 +15,6 @@ namespace AphasiaGreetingCards.Controllers
             return View();
         }
 
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
